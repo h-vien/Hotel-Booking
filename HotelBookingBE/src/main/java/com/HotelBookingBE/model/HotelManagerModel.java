1331ef2a5723fb86238cac285232d8788b259361
minhkycho18 <80151408+minhkycho18@users.noreply.github.com>
2022-04-23 10:47:52 +0700

    23/4/2022 commit BE
    update BE

--- HotelBookingBE/src/main/java/com/HotelBookingBE/model/HotelManagerModel.java
+++ HotelBookingBE/src/main/java/com/HotelBookingBE/model/HotelManagerModel.java
@@ -1,10 +1,17 @@
 package com.HotelBookingBE.model;
 
-public class HotelManagerModel extends UserModel {
+public class HotelManagerModel extends AbstractModel {
+	private long user_id;
 	private String hotelName;
 	private String hotelDescription;
 	private String hotelAddress;
-	private String image;
+	private String image;	
+	public long getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(long user_id) {
+		this.user_id = user_id;
+	}
 	public String getHotelName() {
 		return hotelName;
 	}
