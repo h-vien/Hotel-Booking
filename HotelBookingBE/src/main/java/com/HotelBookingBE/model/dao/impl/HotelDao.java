cc38d5cf51b2d26d5217f62f283177ec6ec29ab3
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2022-05-11 15:55:22 +0700

    fix register member

--- HotelBookingBE/src/main/java/com/HotelBookingBE/model/dao/impl/HotelDao.java
+++ HotelBookingBE/src/main/java/com/HotelBookingBE/model/dao/impl/HotelDao.java
@@ -11,9 +11,9 @@ public class HotelDao extends AbstractDao<HotelModel> implements IHotelDao{
 
 	@Override
 	public void save(HotelModel hotel) {
-		String sql= "insert into hotel(user_id,province_id,hotel_name,hotel_desc,hotel_address,image,createddate,hotel_email,hotel_phone) values (?,?,?,?,?,?,?,?,?)";
+		String sql= "insert into hotel(user_id,province_id,hotel_name,hotel_desc,hotel_address,image,createddate,hotel_email,hotel_phone,room_quantity) values (?,?,?,?,?,?,?,?,?,?)";
 		insert(sql,hotel.getUser_id(),hotel.getProvince_id(),hotel.getHotelName(),
-				hotel.getHotelDescription(),hotel.getHotelAddress(),hotel.getImage(),hotel.getCreatedDate(),hotel.getHotelEmail(),hotel.getHotelPhone());		
+				hotel.getHotelDescription(),hotel.getHotelAddress(),hotel.getImage(),hotel.getCreatedDate(),hotel.getHotelEmail(),hotel.getHotelPhone(),hotel.getRoomQuantity());		
 	}
 
 	@Override
