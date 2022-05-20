0517a33fed328607a480c1461edb853a853152a5
minhkycho18 <80151408+minhkycho18@users.noreply.github.com>
2022-05-20 19:39:05 +0700

    fix birthday

--- HotelBookingBE/src/main/java/com/HotelBookingBE/controller/api/UserApi.java
+++ HotelBookingBE/src/main/java/com/HotelBookingBE/controller/api/UserApi.java
@@ -10,7 +10,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.HotelBookingBE.model.HotelModel;
 import com.HotelBookingBE.model.ShortUserModel;
